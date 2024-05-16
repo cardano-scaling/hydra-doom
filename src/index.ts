27cddab68ceab990446cee653516cdf104660400
Sebastian Nagel <sebastian.nagel@ncoding.at>
2024-05-16 23:47:47 +0200

    Add a button to start deliberately

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,21 @@ declare function callMain(args: string[]): void;
 
 // Glue together callbacks available from doom-wasm
 
+const startButton: HTMLButtonElement | null = document.querySelector("#start");
+
+// TODO: this is flaky and not always happens (in time?)
 Module.onRuntimeInitialized = () => {
     console.log("WASM module loaded");
 
     Module.hydraSend = hydraSend;
     Module.hydraRecv = hydraRecv;
 
+    if (startButton) {
+        startButton.disabled = false;
+    }
+};
+
+startButton?.addEventListener("click", () => {
     var args = [
         "-iwad",
         "doom1.wad",
@@ -21,4 +30,4 @@ Module.onRuntimeInitialized = () => {
         "default.cfg",
     ];
     callMain(args);
-};
+});
