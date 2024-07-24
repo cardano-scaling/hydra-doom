780d705317f3be8397245a6be66da9c5a773f786
Samuel Leathers <samuel.leathers@iohk.io>
2024-07-24 12:34:00 -0400

    use hydra-offline instead of hydra-cluster

--- flake.nix
+++ flake.nix
@@ -28,7 +28,7 @@
       hydraDataDir = "state-hydra";
     in {
       packages = {
-        inherit (inputs.hydra.packages.${system}) hydra-cluster hydra-tui;
+        inherit (inputs.hydra.packages.${system}) hydra-cluster hydra-tui hydra-node;
         inherit (inputs.cardano-node.packages.${system}) cardano-node cardano-cli bech32;
         inherit (inputs.hydra-control-plane.packages.${system}) hydra-control-plane;
         inherit (inputs.doom-wasm.packages.${system}) doom-wasm;
@@ -38,6 +38,33 @@
           text = ''
             rm -rf "${hydraDataDir}"
             ${lib.getExe config.packages.hydra-cluster} --devnet --publish-hydra-scripts --state-directory ${hydraDataDir}
+          '';
+        };
+        hydra-offline-wrapper = pkgs.writeShellApplication {
+          name = "hydra-offline-wrapper";
+          runtimeInputs = [config.packages.cardano-node config.packages.cardano-cli];
+          text = ''
+            rm -rf "${hydraDataDir}"
+            mkdir -p "${hydraDataDir}"
+            cardano-cli address key-gen --normal-key --verification-key-file admin.vk --signing-key-file admin.sk
+            pushd ${hydraDataDir}
+            ${lib.getExe config.packages.hydra-node} gen-hydra-key --output-file hydra
+            curl https://raw.githubusercontent.com/cardano-scaling/hydra/0.17.0/hydra-cluster/config/protocol-parameters.json | jq '.utxoCostPerByte = 0' > protocol-parameters.json
+            cat > utxo.json << EOF
+            {
+              "0000000000000000000000000000000000000000000000000000000000000000#0": {
+                "address": "$(cardano-cli address build --verification-key-file ../admin.vk --testnet-magic 1)",
+                "value": {
+                  "lovelace": 1000000000
+                }
+              }
+            }
+            EOF
+            ${lib.getExe config.packages.hydra-node} offline \
+              --hydra-signing-key hydra.sk \
+              --ledger-protocol-parameters protocol-parameters.json \
+              --initial-utxo utxo.json
+            popd
           '';
         };
         hydra-doom-wrapper = pkgs.writeShellApplication {
@@ -57,7 +84,7 @@
           name = "hydra-tui-wrapper";
           runtimeInputs = [config.packages.hydra-tui];
           text = ''
-            hydra-tui -k state-hydra/wallet.sk
+            hydra-tui -k admin.sk
           '';
         };
         hydra-control-plane-wrapper = pkgs.writeShellApplication {
@@ -71,10 +98,12 @@
         {
           buildInputs = [
             config.packages.hydra-cluster
+            config.packages.hydra-node
             config.packages.hydra-tui
             config.packages.cardano-node
             config.packages.cardano-cli
             config.packages.bech32
+            config.packages.hydra-offline-wrapper
             config.packages.hydra-cluster-wrapper
             config.packages.hydra-doom-wrapper
             pkgs.nodejs
@@ -89,12 +118,15 @@
               #};
 
               processes = {
-                hydra-cluster = {
-                  command = config.packages.hydra-cluster-wrapper;
+                #hydra-cluster = {
+                #  command = config.packages.hydra-cluster-wrapper;
+                #};
+                hydra-offline = {
+                  command = config.packages.hydra-offline-wrapper;
                 };
                 hydra-doom = {
                   command = config.packages.hydra-doom-wrapper;
-                  depends_on."hydra-cluster".condition = "process_started";
+                  depends_on."hydra-offline".condition = "process_started";
                   availability = {
                     restart = "on_failure";
                     backoff_seconds = 2;
@@ -102,7 +134,7 @@
                 };
                 hydra-control-plane = {
                   command = config.packages.hydra-control-plane-wrapper;
-                  depends_on."hydra-cluster".condition = "process_started";
+                  depends_on."hydra-offline".condition = "process_started";
                   availability = {
                     restart = "on_failure";
                     backoff_seconds = 2;
