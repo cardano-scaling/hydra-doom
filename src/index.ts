d6bfb72808b777b079aca2da29f16fcafa1184d2
yHSJ <jgm0100@mcdaniel.edu>
2024-07-26 20:11:14 -0400

    initial integration of poo (wiring, horrible frontend)

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,23 @@
 import { hydraSend, hydraRecv } from "./hydra";
 import { startQueryingAPI } from "./stats";
 import "./styles.css";
+import { generatePooQrUri } from "./keys";
 
 declare function callMain(args: string[]): void;
+
+const startButton: HTMLButtonElement | null = document.querySelector("#start");
+const txPerSecond: HTMLDataListElement | null = document.querySelector("#txps");
+const bytesPerSecond: HTMLDataListElement | null =
+  document.querySelector("#bps");
+const qrContainer: HTMLElement | null = document.querySelector("#qr-container");
+const buttonText: HTMLDataListElement | null =
+  document.querySelector("#button-text");
+console.log({ txPerSecond, bytesPerSecond });
+
+// Stuff for POO
+let qrCode = await generatePooQrUri();
+console.log(qrCode);
+let qrShown = false;
 
 // Glue together callbacks available from doom-wasm
 (window as any).hydraSend = hydraSend;
@@ -19,26 +34,46 @@ const commonArgs = [
 ];
 
 // Start a new game
-const startButton: HTMLButtonElement | null = document.querySelector("#start");
 startButton?.addEventListener("click", () => {
-  // Hide the button
-  if (startButton) {
-    startButton.style.display = "none";
-  }
-  callMain(commonArgs.concat(["-hydra-send"]));
+  !!qrCode && !qrShown ? showQrCode() : startGame();
 });
 
-// Watch game
-// FIXME: prevent /new_game in hydra.ts
-const params = new URLSearchParams(window.location.search);
-if (params.get("watch") != null) {
-  // Hide the button
-  if (startButton) {
-    startButton.style.display = "none";
+function showQrCode() {
+  qrShown = true;
+
+  const img = document.createElement("img");
+  img.src = qrCode!;
+  qrContainer?.appendChild(img);
+
+  // We can add polling here and hide the button, but for now we'll just let the user click the button again
+  if (buttonText) {
+    buttonText.innerText = "Start Game";
   }
-  setTimeout(() => {
-    callMain(commonArgs.concat("-hydra-recv"));
-  }, 1000);
+}
+
+function startGame() {
+  {
+    if (startButton) {
+      startButton.style.display = "none";
+    }
+    if (qrContainer) {
+      qrContainer.style.display = "none";
+    }
+    callMain(commonArgs.concat(["-hydra-send"]));
+  }
+
+  // Watch game
+  // FIXME: prevent /new_game in hydra.ts
+  const params = new URLSearchParams(window.location.search);
+  if (params.get("watch") != null) {
+    // Hide the button
+    if (startButton) {
+      startButton.style.display = "none";
+    }
+    setTimeout(() => {
+      callMain(commonArgs.concat("-hydra-recv"));
+    }, 1000);
+  }
 }
 
 startQueryingAPI();
