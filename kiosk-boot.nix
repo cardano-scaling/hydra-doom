b2531dceb11572e43729c71dfab16c128ba29912
Samuel Leathers <samuel.leathers@iohk.io>
2024-07-30 20:30:14 -0400

    kiosk-boot: add nginx virtual hosts for offline and remote play

--- kiosk-boot.nix
+++ kiosk-boot.nix
@@ -6,7 +6,8 @@
   system,
   ...
 }: let
-  prodImage = false;
+  # change to false when running in qemu
+  prodImage = true;
 in {
   imports = [(modulesPath + "/installer/cd-dvd/installation-cd-graphical-gnome.nix")];
 
@@ -48,6 +49,7 @@ in {
         cardano-node
         cardano-cli
         bech32
+        default
       ]
       ++ (with pkgs; [
         gnome3.adwaita-icon-theme
@@ -57,6 +59,7 @@ in {
         openssl
         usbutils
         util-linux
+        google-chrome
       ]);
 
   };
@@ -130,72 +133,38 @@ in {
     gnupg.agent.enable = true;
   };
 
+  networking.hosts = lib.mkForce {
+    "127.0.0.1" = [ "localhost" "doom-remote.local" "doom-offline.local" ];
+    "::1" = [ "localhost" "doom-remote.local" "doom-offline.local" ];
+  };
   services = {
-    udev.extraRules = ''
-      SUBSYSTEMS=="usb", ATTRS{idVendor}=="2581", ATTRS{idProduct}=="1b7c", MODE="0660", TAG+="uaccess", TAG+="udev-acl"
-      SUBSYSTEMS=="usb", ATTRS{idVendor}=="2581", ATTRS{idProduct}=="2b7c", MODE="0660", TAG+="uaccess", TAG+="udev-acl"
-      SUBSYSTEMS=="usb", ATTRS{idVendor}=="2581", ATTRS{idProduct}=="3b7c", MODE="0660", TAG+="uaccess", TAG+="udev-acl"
-      SUBSYSTEMS=="usb", ATTRS{idVendor}=="2581", ATTRS{idProduct}=="4b7c", MODE="0660", TAG+="uaccess", TAG+="udev-acl"
-      SUBSYSTEMS=="usb", ATTRS{idVendor}=="2581", ATTRS{idProduct}=="1807", MODE="0660", TAG+="uaccess", TAG+="udev-acl"
-      SUBSYSTEMS=="usb", ATTRS{idVendor}=="2581", ATTRS{idProduct}=="1808", MODE="0660", TAG+="uaccess", TAG+="udev-acl"
-      SUBSYSTEMS=="usb", ATTRS{idVendor}=="2c97", ATTRS{idProduct}=="0000", MODE="0660", TAG+="uaccess", TAG+="udev-acl"
-      SUBSYSTEMS=="usb", ATTRS{idVendor}=="2c97", ATTRS{idProduct}=="0001", MODE="0660", TAG+="uaccess", TAG+="udev-acl"
-      SUBSYSTEMS=="usb", ATTRS{idVendor}=="2c97", ATTRS{idProduct}=="0004", MODE="0660", TAG+="uaccess", TAG+="udev-acl"
-      KERNEL=="hidraw*", SUBSYSTEM=="hidraw", MODE="0660", GROUP="plugdev", ATTRS{idVendor}=="2c97"
-      KERNEL=="hidraw*", SUBSYSTEM=="hidraw", MODE="0660", GROUP="plugdev", ATTRS{idVendor}=="2581"
-      ACTION=="add", SUBSYSTEM=="thunderbolt", ATTR{authorized}=="0", ATTR{authorized}="1"
-    '';
+    #cage = {
+    #  enable = true;
+    #  program = "${pkgs.google-chrome}/bin/google-chrome-stable --app=http://doom-remote.local";
+    #  #program = "${pkgs.google-chrome}/bin/google-chrome-stable";
+    #  user = "nixos";
+    #};
+    nginx = {
+      enable = true;
+      virtualHosts = {
+        "doom-remote.local" = {
+          root = self.packages.${system}.hydra-doom-static-remote;
+          extraConfig = ''
+            disable_symlinks off;
+            try_files $uri $uri /index.html;
+          '';
+        };
+        "doom-offline.local" = {
+          root = self.packages.${system}.hydra-doom-static-local;
+          extraConfig = ''
+            disable_symlinks off;
+            try_files $uri $uri /index.html;
+          '';
+        };
+      };
+    };
   };
 
-  systemd.user.services.dconf-defaults = {
-    script = let
-      dconfDefaults = pkgs.writeText "dconf.defaults" ''
-        [org/gnome/desktop/background]
-        color-shading-type='solid'
-        picture-options='zoom'
-        picture-uri='${./assets/logo.png}'
-        primary-color='#000000000000'
-        secondary-color='#000000000000'
-
-        [org/gnome/desktop/lockdown]
-        disable-lock-screen=true
-        disable-log-out=true
-        disable-user-switching=true
-
-        [org/gnome/desktop/notifications]
-        show-in-lock-screen=false
-
-        [org/gnome/desktop/screensaver]
-        color-shading-type='solid'
-        lock-delay=uint32 0
-        lock-enabled=false
-        picture-options='zoom'
-        picture-uri='${./assets/logo.png}'
-        primary-color='#000000000000'
-        secondary-color='#000000000000'
-
-        [org/gnome/settings-daemon/plugins/media-keys]
-        custom-keybindings=['/org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0/']
-
-        [org/gnome/settings-daemon/plugins/media-keys/custom-keybindings/custom0]
-        binding='<Primary><Alt>t'
-        command='kgx'
-        name='console'
-
-        [org/gnome/settings-daemon/plugins/power]
-        idle-dim=false
-        power-button-action='interactive'
-        sleep-inactive-ac-type='nothing'
-
-        [org/gnome/shell]
-        welcome-dialog-last-shown-version='41.2'
-      '';
-    in ''
-      ${pkgs.dconf}/bin/dconf load / < ${dconfDefaults}
-    '';
-    wantedBy = ["graphical-session.target"];
-    partOf = ["graphical-session.target"];
-  };
 
   system = {
     stateVersion = lib.versions.majorMinor lib.version;
