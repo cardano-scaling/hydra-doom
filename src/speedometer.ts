1e78f81f64cd921c21cf58fbecc374d0efeeaad8
Sebastian Nagel <sebastian.nagel@ncoding.at>
2024-08-08 23:15:34 +0200

    Hack a somewhat configurable speedometer

--- src/speedometer.ts
+++ src/speedometer.ts
@@ -1,13 +1,16 @@
 const speedometerTick: HTMLDivElement | null = document.querySelector("[data-speedometer-value]");
+const speedometerMax: HTMLDivElement | null = document.querySelector(".speedometer-max");
 
-// Map range from [0, 500] to [0, 180]
+// Map a value from one range to another
 function mapRange(value : number, inMin : number, inMax : number, outMin : number, outMax : number) {
   return ((value - inMin) * (outMax - outMin)) / (inMax - inMin) + outMin;
 }
 
+export const MAX_SPEED = 50;
+speedometerMax!.innerText = MAX_SPEED.toString();
 
-// Set the speedometer value in the range [0, 500]
+// Set the speedometer value in the range [0, MAX_SPEED]
 export function setSpeedometerValue(value: number) {
-  const degree = mapRange(value, 0, 500, 0, 180);
+  const degree = mapRange(value, 0, MAX_SPEED, 0, 180);
   speedometerTick!.style.transform = `rotate(${degree}deg)`;
 }
