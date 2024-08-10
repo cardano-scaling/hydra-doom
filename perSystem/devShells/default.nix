56f757a80728b0862c36347aa7e55decf94ba7f6
Sebastian Nagel <sebastian.nagel@ncoding.at>
2024-08-10 09:22:58 +0200

    Make devshell not require correct build of hydra-doom
    Dropping hydra-doom-wrapper et al makes the devshell load up quicker and
    also evaluate on not currently building src tree.

--- perSystem/devShells/default.nix
+++ perSystem/devShells/default.nix
@@ -1,31 +1,30 @@
 { inputs, ... }: {
-  perSystem = {config, system, pkgs, lib, ...}: {
+  perSystem = { config, system, pkgs, lib, ... }: {
     devShells.default = pkgs.mkShell
       {
-        nativeBuildInputs = let
-          inherit (inputs.sops-nix.packages."${system}") sops-import-keys-hook ssh-to-pgp sops-init-gpg-key;
-        in [
-          config.packages.hydra-cluster
-          config.packages.hydra-node
-          config.packages.hydra-tui
-          config.packages.cardano-node
-          config.packages.cardano-cli
-          config.packages.bech32
-          config.packages.hydra-offline-wrapper
-          config.packages.hydra-cluster-wrapper
-          config.packages.hydra-doom-wrapper
-          pkgs.nodejs
-          pkgs.jq
-          pkgs.age
-          pkgs.ssh-to-age
-          pkgs.pwgen
-          pkgs.just
-          inputs.colmena.packages.${system}.colmena
-          sops-import-keys-hook
-          ssh-to-pgp
-          sops-init-gpg-key
-          config.treefmt.build.wrapper
-        ];
+        nativeBuildInputs =
+          let
+            inherit (inputs.sops-nix.packages."${system}") sops-import-keys-hook ssh-to-pgp sops-init-gpg-key;
+          in
+          [
+            config.packages.hydra-cluster
+            config.packages.hydra-node
+            config.packages.hydra-tui
+            config.packages.cardano-node
+            config.packages.cardano-cli
+            config.packages.bech32
+            pkgs.nodejs
+            pkgs.jq
+            pkgs.age
+            pkgs.ssh-to-age
+            pkgs.pwgen
+            pkgs.just
+            inputs.colmena.packages.${system}.colmena
+            sops-import-keys-hook
+            ssh-to-pgp
+            sops-init-gpg-key
+            config.treefmt.build.wrapper
+          ];
       };
-    };
+  };
 }
