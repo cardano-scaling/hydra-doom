d4b20f9185e72d004c5756794928259b9c8e46b1
Selvio Perez <selvio89@hotmail.com>
2024-08-12 13:23:43 -0500

    feat: leaderboard tables

--- src/types.ts
+++ src/types.ts
@@ -20,5 +20,33 @@ export const recordValueToAssets = (value: Record<string, number>): Assets =>
       acc[key] = BigInt(value);
       return acc;
     },
-    {} as Record<Unit, bigint>,
+    {} as Record<Unit, bigint>
   );
+
+export type LeaderboardEntry = [string, number];
+
+export interface PlayerStats {
+  [key: string]: number;
+}
+
+export interface PlayerPlayTime {
+  [key: string]: number[];
+}
+
+export interface GameStatistics {
+  total_games: number;
+  active_games: number;
+  transactions: number;
+  bytes: number;
+  kills: PlayerStats;
+  total_kills: number;
+  kills_leaderboard: LeaderboardEntry[];
+  items: PlayerStats;
+  total_items: number;
+  items_leaderboard: LeaderboardEntry[];
+  secrets: PlayerStats;
+  total_secrets: number;
+  secrets_leaderboard: LeaderboardEntry[];
+  player_play_time: PlayerPlayTime;
+  total_play_time: number;
+}
