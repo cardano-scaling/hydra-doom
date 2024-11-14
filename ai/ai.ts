54325a339578a02d53f0ea162bbe7a3fee7f94ca
Pi Lanningham <pi.lanningham@gmail.com>
2024-11-14 17:08:04 -0500

    Add an AI agent + image

---
+++ ai/ai.ts
@@ -0,0 +1,100 @@
+// Run as a node process to run a dedicated doom server
+
+import { readFile } from "node:fs/promises";
+import { HydraMultiplayer } from "utils/hydra-multiplayer";
+import { Lucid, toHex, fromHex } from "lucid-cardano";
+import * as bech32 from "bech32-buffer";
+import * as ed25519 from "@noble/ed25519";
+import { blake2b } from "@noble/hashes/blake2b";
+
+// TODO: should we generate this key, like the UI does? no reason we need to keep it around
+let done = false;
+const lucid = await Lucid.new(undefined, "Preprod");
+const adminKeyFile = process.env.BOT_KEY_FILE ?? "bot.sk";
+const adminKey = JSON.parse((await readFile(adminKeyFile)).toString());
+const privateKeyBytes = adminKey.cborHex.slice(4);
+const sessionKeyBech32 = bech32.encode("ed25519_sk", fromHex(privateKeyBytes));
+
+const publicKeyBytes = await ed25519.getPublicKeyAsync(privateKeyBytes);
+const publicKeyHashBytes = blake2b(publicKeyBytes, { dkLen: 224 / 8 });
+const publicKeyHashHex = toHex(publicKeyHashBytes);
+const keys = {
+  sessionKeyBech32,
+  privateKeyBytes,
+  privateKeyHex: toHex(privateKeyBytes),
+  publicKeyBytes,
+  publicKeyHex: toHex(publicKeyBytes),
+  publicKeyHashBytes,
+  publicKeyHashHex,
+  address: lucid.utils.credentialToAddress({
+    type: "Key",
+    hash: publicKeyHashHex,
+  }),
+};
+console.log("Address: ", keys.address);
+
+const { default: createModule } = await import("../websockets-doom.js");
+const module = await createModule({
+  locateFile: (path, scripts) => {
+    console.log(scripts + "public/" + path);
+    return scripts + "public/" + path;
+  },
+  noInitialRun: true,
+  preRun: function (mod: any) {
+    const files = ["freedoom2.wad", "default.cfg", "Cardano.wad"];
+    files.forEach((file) => {
+      mod.FS!.createPreloadedFile("/", file, "../public/" + file, true, true);
+    });
+  },
+  canvas: null as any,
+  print: (text: string) => {
+    console.log("stdout:", text);
+  },
+  printErr: (text: string) => {
+    console.error("stderr:", text);
+  },
+  onExit: () => {
+    console.log("Game exited");
+    done = true;
+  },
+});
+global.Module = module;
+global.HydraMultiplayer = new HydraMultiplayer(
+  keys,
+  "http://localhost:4001",
+  module,
+);
+
+// TODO: modify new-game transaction to record # of players
+// TODO: watch for new-game transaction to decide if we're participating in this game
+
+// TODO: generate a fun pet name
+const args = [
+  "-connect",
+  "1",
+  "-deathmatch",
+  "-ai",
+  "-iwad",
+  "freedoom2.wad",
+  "-file",
+  "Cardano.wad",
+  "-nodraw",
+  "-nomouse",
+  "-nograbmouse",
+  "-nogui",
+  "-nomusic",
+  "-nosound",
+  "-config",
+  "default.cfg",
+];
+try {
+  module.callMain(args);
+} catch (e) {
+  console.error(e);
+}
+
+while (!done) {
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+}
+
+process.exit(0);
