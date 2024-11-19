364c77ac6c89becac7619d7a2db6361e8ccd4106
Selvio Perez <selvio89@hotmail.com>
2024-11-19 18:35:15 -0500

    Add new region

--- src/constants.ts
+++ src/constants.ts
@@ -5,5 +5,5 @@ export const HANDLE_CACHE_KEY = "player-handle-cache";
 export const HYDRA_DOOM_SESSION_KEY = "hydra-doom-session-key";
 export const MAX_SPEED = 40;
 export const REGION = import.meta.env.VITE_REGION;
-export const REGIONS = ["us-east-1"];
+export const REGIONS = ["us-east-1", "eu-central-1"];
 export const TIC_RATE_MAGIC = 35; // 35 is the ticrate in DOOM WASM they use to calculate time.
