91d9c4827e5fbb850d760bb20a8f995c45b7a8b7
Selvio Perez <selvio89@hotmail.com>
2024-12-01 10:37:07 -0500

    feat: stars tooltip

--- src/components/SessionStats/SessionStats.tsx
+++ src/components/SessionStats/SessionStats.tsx
@@ -8,6 +8,7 @@ import { fetchSessionStats } from "../../utils/requests";
 import { SessionStatsInterface } from "../../types";
 import { FaStar } from "react-icons/fa6";
 import cx from "classnames";
+import { Tooltip } from "react-tooltip";
 
 const SessionStats: FC<Pick<StatsCardProps, "size" | "titleAlign">> = ({
   size,
@@ -27,18 +28,33 @@ const SessionStats: FC<Pick<StatsCardProps, "size" | "titleAlign">> = ({
 
   const stats = [
     {
+      id: "games-played",
       label: "Games Played:",
       value: game_started,
       showStar: game_started > 0,
       starColor: game_started >= 4 ? "text-yellow-400" : "text-white",
+      tooltip: (
+        <Tooltip anchorSelect="#games-played">
+          <div>Pass qualifier: 1</div>
+          <div>Achievement: 4</div>
+        </Tooltip>
+      ),
     },
     {
+      id: "kills",
       label: "Kills:",
       value: kill,
       showStar: kill >= 25,
       starColor: kill >= 50 ? "text-yellow-400" : "text-white",
+      tooltip: (
+        <Tooltip anchorSelect="#kills">
+          <div>Pass qualifier: 25</div>
+          <div>Achievement: 50</div>
+        </Tooltip>
+      ),
     },
     {
+      id: "deaths",
       label: "Deaths:",
       value: death,
       showStar: false,
@@ -46,20 +62,23 @@ const SessionStats: FC<Pick<StatsCardProps, "size" | "titleAlign">> = ({
     },
   ];
 
-  const formattedStats = stats.map(({ label, value, showStar, starColor }) => {
-    const formattedValue = formatNumber(value);
-    return {
-      label,
-      value: showStar ? (
-        <div className="flex items-center gap-4 w-fit">
-          <div className="w-5">{formattedValue}</div>
-          <FaStar className={cx("text-base", starColor)} />
-        </div>
-      ) : (
-        formattedValue
-      ),
-    };
-  });
+  const formattedStats = stats.map(
+    ({ label, value, showStar, starColor, tooltip, id }) => {
+      const formattedValue = formatNumber(value);
+      return {
+        label,
+        value: showStar ? (
+          <div className="flex items-center gap-4 w-fit">
+            <div className="w-5">{formattedValue}</div>
+            <FaStar className={cx("text-base", starColor)} id={id} />
+            {tooltip}
+          </div>
+        ) : (
+          formattedValue
+        ),
+      };
+    },
+  );
 
   if (!accountData) return null;
 
