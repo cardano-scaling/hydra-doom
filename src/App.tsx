cb0b8555b409ab8f08122f27776b00a448c9aad8
Calvin Koepke <hello@calvinkoepke.com>
2025-01-15 13:41:19 -0700

    chore: add basic wallet funcs

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,39 @@
-import { useState } from "react";
+import { useState, lazy, useMemo } from "react";
 import InitialView from "./components/InitialView";
 import GameView from "./components/GameView";
 import AppContextProvider from "./context/AppContextProvider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
+const LazyClaimView = lazy(() => import("./components/ClaimView"));
+
 const queryClient = new QueryClient();
 
 export default function App() {
+  const pathSegments = window.location.hash.split("/").filter(Boolean);
+  const url = pathSegments?.[1];
   const [isGameStarted, setIsGameStarted] = useState(false);
 
-  const startGame = () => {
-    setIsGameStarted(true);
-    window.history.replaceState({}, document.title, "/");
-  };
+  const normalView = useMemo(
+    () => (
+      <AppContextProvider>
+        {isGameStarted ? (
+          <GameView />
+        ) : (
+          <InitialView
+            startGame={() => {
+              setIsGameStarted(true);
+              window.history.replaceState({}, document.title, "/");
+            }}
+          />
+        )}
+      </AppContextProvider>
+    ),
+    [isGameStarted],
+  );
 
   return (
     <QueryClientProvider client={queryClient}>
-      <AppContextProvider>
-        {isGameStarted ? <GameView /> : <InitialView startGame={startGame} />}
-      </AppContextProvider>
+      {url === "claim-reward" ? <LazyClaimView /> : normalView}
     </QueryClientProvider>
   );
 }
